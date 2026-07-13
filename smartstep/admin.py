8e156183dfe65e379be7156e8533bfb2eaa11d49
jiyeoniii <dlwldus0705@naver.com>
2026-07-13 17:14:59 +0900

    feat: 코스/챕터/스텝/진도 모델 및 Admin 등록

--- smartstep/admin.py
+++ smartstep/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Chapter, Course, Progress, Step
+
+
+class ChapterInline(admin.TabularInline):
+    model = Chapter
+    extra = 1
+    fields = ['order', 'emoji', 'name', 'slug', 'goal', 'minutes']
+
+
+class StepInline(admin.TabularInline):
+    model = Step
+    extra = 1
+    fields = ['order', 'instruction', 'html']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['order', '__str__', 'slug', 'chapter_count']
+    list_display_links = ['__str__']
+    inlines = [ChapterInline]
+
+    @admin.display(description='챕터 수')
+    def chapter_count(self, obj):
+        return obj.chapters.count()
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ['order', '__str__', 'course', 'slug', 'minutes', 'step_count']
+    list_display_links = ['__str__']
+    list_filter = ['course']
+    inlines = [StepInline]
+
+    @admin.display(description='스텝 수')
+    def step_count(self, obj):
+        return obj.steps.count()
+
+
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
+    list_display = ['session_key', 'chapter', 'last_step', 'is_completed', 'updated_at']
+    list_filter = ['is_completed', 'chapter']
+    readonly_fields = ['updated_at']
